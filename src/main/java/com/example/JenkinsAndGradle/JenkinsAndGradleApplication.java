3291f8f53cbe93ccf59b4321da6ca245b1458156
Cho Jin Ho <whwlsgh1111@naver.com>
2023-10-04 16:26:07 +0900

    Initial commit

---
+++ src/main/java/com/example/JenkinsAndGradle/JenkinsAndGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsAndGradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsAndGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsAndGradleApplication.class, args);
+	}
+
+}
